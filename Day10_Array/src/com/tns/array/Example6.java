0396deff6afc61cd9e64c7a9522f799108373bf7
user <user@DESKTOP-2K37FSJ>
2022-04-08 18:49:39 +0530

    first commit

---
+++ Day10_Array/src/com/tns/array/Example6.java
@@ -0,0 +1,14 @@
+package com.tns.array;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		int arr[][]= {{10,20,30},{40,50,60}};
+		System.out.println(arr[0][0]);
+		System.out.println(arr[1][0]);
+		System.out.println(arr[1][1]);
+		System.out.println(arr[1][2]);
+
+	}
+
+}
